c5a39443b7ab9a57bf9bf710b816621b006b5954
Alessandro Ghedini <al3xbio@gmail.com>
2011-03-21 21:47:00 +0100

    first commit

---
+++ lib/App/gist.pm
@@ -0,0 +1,34 @@
+package App::gist;
+
+use warnings;
+use strict;
+
+=head1 NAME
+
+App::gist - Module to do something
+
+=head1 SYNOPSIS
+
+Synopsis section
+
+    use App::gist;
+
+    ...
+
+=head1 AUTHOR
+
+Alessandro Ghedini <alexbio@cpan.org>
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2011 Alessandro Ghedini.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+=cut
+
+1; # End of App::gist
